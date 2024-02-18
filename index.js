ecf81325931fea66d5672df9b4a7eb57b9d27c82
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-02-18 15:32:16 -0500

    changes fo adding a user input table, settings popup and fetching the emission coeffecient

--- index.js
+++ index.js
@@ -1,46 +1,101 @@
-// code to display settings popup when icon is clied and close when clicked on smewhere else
-function toggleSettings() {
-  document.getElementById("popupBg").style.display = "block";
+// variable to store all the data
+const fleetData = [];
+
+optionsForProvinces = [
+    'Alberta','British Columbia','Manitoba','New Brunswick','Newfoundland and Labrador',
+    'Nova Scotia','Ontario','Prince Edward Island','Quebec','Saskatchewan',
+    'Northwest Territories','Nunavut','Yukon'
+]
+
+function createOptions() {
+    // make Ontario as a default selected option
+    optionsForProvinces.forEach((item)=>{
+        var option = document.createElement('option');
+        option.value = item;
+        option.text = item;
+        if(item === 'Ontario') {
+            option.selected = true;
+        }
+        document.getElementById('provinceSelect').appendChild(option);
+    })    
 }
-function closeSettings() {
-  document.getElementById("popupBg").style.display = "none";
 
+window.onload = createTable;
+
+function createTable() {
+    createHeaderRow();
+    if (fleetData.length === 0) {
+        createEmptyTableRow();
+    } else {
+        createTableRows();
+    }
 }
-// if popup is opened and user clicks randomly on the scree, the popup should be closed
-window.addEventListener("click") = function (event) {
-  console.log(event.target);
-  const popup = document.querySelector(".popupBg");
-  if (event.target == popup) {
-    popup.classList.remove("show");
-  }
+
+
+//  write code to geenerate th for myTable
+function createHeaderRow() {
+    var table = document.getElementById("myTable");
+    var thead = table.createTHead();
+    var headerRow = thead.insertRow(-1);
+    headerRow.insertCell(-1).innerHTML = "Description";
+    headerRow.insertCell(-1).innerHTML = "Type";
+    headerRow.insertCell(-1).innerHTML = "Year";
+    headerRow.insertCell(-1).innerHTML = "Make";
+    headerRow.insertCell(-1).innerHTML = "Model";
+    headerRow.insertCell(-1).innerHTML = "Annual VKT";
+    headerRow.insertCell(-1).innerHTML = "Annual Fuel";
+    headerRow.insertCell(-1).innerHTML = "Fuel Type";
+    headerRow.insertCell(-1).innerHTML = "Flex-Fuel";
+    headerRow.insertCell(-1).innerHTML = "Quantity";
 }
 
-// Retrieve data from local storage or use an empty array
-const storedData = JSON.parse(localStorage.getItem("fleetData")) || [];
+function createEmptyTableRow() {
+        var table = document.getElementById("myTable");
+        var tbody = document.createElement("tbody");
+        let row = tbody.insertRow(-1);
+        row.id = "emptyRow";
+        let cell = row.insertCell(-1);
+        cell.colSpan = 10;
+        cell.innerHTML = "No records Added";
+        cell.style.textAlign = "center";
+        table.appendChild(tbody);
+}
 
-// Initialize DataTable
-const dataTable = $("#fleetTable").DataTable({
-  data: storedData,
-  columns: [
-    { data: "id" },
-    { data: "name" },
-    { data: "vehicleType" },
-    { data: "fuelType" },
-  ],
-});
+function createTableRows() {
+    var table = document.getElementById('myTable')
+    fleetData.forEach(fleet => {
+        createRow(fleet) 
+    });
+}
 
-// Function to add a new row
-function addNewRow() {
-  const newRow = {
-    id: dataTable.data().count() + 1,
-    name: prompt("Enter Name:"),
-    vehicleType: prompt("Enter Vehicle Type:"),
-    fuelType: prompt("Enter Fuel Type:"),
-  };
 
-  dataTable.row.add(newRow).draw();
+function yesOrNo(value) {
+    console.log(value);
+    if(value === 'Yes') {
+        return 'True';
+    } else {
+        return 'False';
+    }
+}
 
-  // Update the local storage data
-  storedData.push(newRow);
-  localStorage.setItem("fleetData", JSON.stringify(storedData));
+function createRow(fleet) {
+    var table = document.getElementById("myTable");
+    // delete empty row if exists
+    var emptyRow = document.getElementById("emptyRow");
+    if (emptyRow) {
+        emptyRow.remove();
+    }
+    var rowRecord = Object.values(fleet)
+    var tbody = table.tBodies[0];
+    let row = tbody.insertRow(-1);
+    for(let i = 0; i < rowRecord.length; i++) {
+        let cell = row.insertCell(-1);
+        cell.innerHTML = rowRecord[i];
+    }
+    // add a onclick function
+    row.onclick = function() {
+        console.log(fleet);
+        openModalToAddRow(fleet);
+        console.log(row);
+    }
 }
---
+++ main.js
@@ -0,0 +1,122 @@
+function openSettingsModal() {
+  var modal = document.getElementById("settingsModal");
+  var overlay = document.getElementById("overlay");
+  modal.style.display = "flex";
+  overlay.style.display = "block";
+  document.body.style.overflow = "hidden"; // Prevent scrolling of main content
+  createOptions();
+  updateEmissionCoefficient();
+}
+
+// function to update the coefficient
+function updateEmissionCoefficient() {
+  var selectedProvince = document.getElementById("provinceSelect").value;
+  var emissionCoefficient = getEmissionCoefficient(selectedProvince);
+  document.getElementById("emissionCoefficient").value = emissionCoefficient;
+}
+
+// function to get the emission coefficient
+function getEmissionCoefficient(selectedProvince) {
+  for (var i = 0; i < provincialWfficnienyCoefficient.length; i++) {
+    if (provincialWfficnienyCoefficient[i].p === selectedProvince) {
+      return provincialWfficnienyCoefficient[i].c;
+    }
+  }
+  return 0;
+}
+
+function closeSettingsModal() {
+  var modal = document.getElementById("settingsModal");
+  var overlay = document.getElementById("overlay");
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  document.body.style.overflow = ""; // Restore scrolling of main content
+}
+
+// modal opeing at setup for adding a row
+// function to open modal for adding a row at setup
+function openModalToAddRow(fleet) {
+  var modal = document.getElementById("dataRowModal");
+  var overlay = document.getElementById("overlay");
+  modal.style.display = "block";
+  overlay.style.display = "block";
+  document.body.style.overflow = "hidden"; // Prevent scrolling of main content
+  // set up the data fields in the modal
+  if (fleet) {
+    document.getElementById("modalDescription").value = fleet["description"];
+    document.getElementById("modalType").value = fleet["type"];
+    document.getElementById("modalYear").value = fleet["year"];
+    document.getElementById("modalMake").value = fleet['make'];
+    document.getElementById("modalModel").value = fleet['model'];
+    document.getElementById("modalAnnualVKT").value = fleet['vkt'];
+    document.getElementById("modalAnnualFuel").value = fleet['fuel_used'];
+    document.getElementById("modalFuelType").value = fleet['fuel_type'];
+    document.getElementById("modalFlexFuel").value = yesOrNo(fleet['flex_fuel']);
+    document.getElementById("modalQuantity").value = fleet['quantity'];
+  }
+}
+
+// function to close modal
+function closeModal() {
+  var modal = document.getElementById("dataRowModal");
+  var overlay = document.getElementById("overlay");
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  document.body.style.overflow = ""; // Restore scrolling of main content
+  resetModalForm();
+}
+// function to clear out form fields when closing or canceling
+function resetModalForm() {
+  document.getElementById("modalDescription").value = "";
+  document.getElementById("modalType").value = "";
+  document.getElementById("modalYear").value = "";
+  document.getElementById("modalMake").value = "";
+  document.getElementById("modalModel").value = "";
+  document.getElementById("modalAnnualVKT").value = "";
+  document.getElementById("modalAnnualFuel").value = "";
+  document.getElementById("modalFuelType").value = "";
+  document.getElementById("modalFlexFuel").value = "";
+  document.getElementById("modalQuantity").value = "";
+}
+
+// Function to handle submit button on click event
+function submitData() {
+  var modal = document.getElementById("dataRowModal");
+  var overlay = document.getElementById("overlay");
+  if (validateInput()) {
+    addNewRowToTable();
+    modal.style.display = "none";
+    overlay.style.display = "none";
+    document.body.style.overflow = ""; // Restore scrolling of main content
+    resetModalForm();
+  }
+}
+
+// function to validate input
+function validateInput() {
+  console.log("Validating input");
+  return true;
+}
+
+document.getElementById('dataRowModal').addEventListener('input', function(event) {
+  const input = event.target;
+});
+
+function addNewRowToTable()  {
+  console.log("Adding new row to table");
+  console.log(document.getElementById("modalDescription").value);
+  let fleet ={
+    description: document.getElementById("modalDescription").value,
+    type: document.getElementById("modalType").value,
+    year: document.getElementById("modalYear").value,
+    make: document.getElementById("modalMake").value,
+    model: document.getElementById("modalModel").value,
+    vkt: document.getElementById("modalAnnualVKT").value,
+    fuel_used: document.getElementById("modalAnnualFuel").value,
+    fuel_type: document.getElementById("modalFuelType").value,
+    flex_fuel: yesOrNo(document.getElementById("modalFlexFuel").value),
+    quantity: document.getElementById("modalQuantity").value
+  } 
+  fleetData.push(fleet);
+  createRow(fleet);
+}
