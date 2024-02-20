cd9bef388af4ce0d241a14f2acfbd6a01c73f38d
Riddhi <riddhijobanputra5@gmail.com>
2024-02-20 17:17:51 -0500

    Action Savings Page

---
+++ actionsavings.js
@@ -0,0 +1,12 @@
+const individualSavingsBtn = document.querySelector('.action-savings button:nth-of-type(1)');
+const overallSavingsBtn = document.querySelector('.action-savings button:nth-of-type(2)');
+
+individualSavingsBtn.addEventListener('click', () => {
+    individualSavingsBtn.classList.add('active');
+    overallSavingsBtn.classList.remove('active');
+});
+
+overallSavingsBtn.addEventListener('click', () => {
+    overallSavingsBtn.classList.add('active');
+    individualSavingsBtn.classList.remove('active');
+});
