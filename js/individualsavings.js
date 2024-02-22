bb39022d16eb6b9bc2e6bcf0e1fa3cd6039d8798
Riddhi <riddhijobanputra5@gmail.com>
2024-02-22 07:15:50 -0500

    Added JavaScript logic for displaying individual action savings

--- js/individualsavings.js
+++ js/individualsavings.js
@@ -12,3 +12,39 @@ overallSavingsBtn.addEventListener('click', () => {
 });
 
 var fleetData = getFleetData();
+console.log(fleetData);
+
+populateContainer();
+
+function populateContainer() {
+    const container = document.querySelector(".vehicle-list");
+    fleetData.forEach(item => {
+        // Create divs for each item
+        const div = document.createElement("div");
+        div.classList.add("vehicle-item");
+        // Add 2 side by side divs of equal width for the description and options
+        let descDiv = createDescriptionDiv(item);
+        div.appendChild(descDiv);
+        container.appendChild(div);
+ 
+        // the options div should only be visible when either the row is hovered or clicked
+        div.addEventListener("click", () => {
+                const infoDiv = document.querySelector(".savings-info");
+                document.querySelectorAll('.vehicle-item').forEach(item => {
+                    item.classList.remove('selected');
+                });
+                div.classList.add('selected');
+                infoDiv.classList.remove('hidden');    
+                console.log(item['selectedOption']);
+                document.querySelector('.option-selected').innerHTML = "Option Selected :" + item['selectedOption'];          
+        });
+    });
+}
+
+function createDescriptionDiv(item) {
+    const descDiv = document.createElement("div");
+    descDiv.classList.add("description");
+    descDiv.textContent = item['description'] + " - " + item['type'] + " - " +
+        item['year'] + " - " + item['make'] + " - " + item['model'];
+    return descDiv;
+}
