a66cba51e55283a9811bba78a4fd921620252a28
iveriquenkayilu <iveriquenkayilu@gmail.com>
2024-02-27 21:36:00 -0500

    Added charts js

--- js/charts.js
+++ js/charts.js
@@ -1,20 +1,30 @@
 const canvas = document.getElementById("myChart");
 const ctx = canvas.getContext("2d");
+
 var gradient = ctx ? ctx.createLinearGradient(0, 0, 600, 0) : null;
 if (gradient) {
   gradient.addColorStop(0, "#07354d");
   gradient.addColorStop(1, "#95cdeb");
 }
+
+const canvas2 = document.getElementById("myChart2");
+const ctx2 = canvas2.getContext("2d");
+var gradient2 = ctx2 ? ctx2.createLinearGradient(0, 0, 600, 0) : null;
+if (gradient2) {
+  gradient2.addColorStop(0, "#07354d");
+  gradient2.addColorStop(1, "#95cdeb");
+}
+
 var vehicles = [];
 var emmisionCoefficient = localStorage.getItem("emissionCoefficient") || "30";
 
 window.onload = () => {
   vehicles = getFleetData();
-  vehicles.forEach(async (element) => {
-    await calculateEmissions(element);
-    calculateCharts();
+  vehicles.forEach( (element) => {
+     calculateEmissions(element); 
   });
-  console.log(vehicles);
+  calculateCharts();
+  //console.log(vehicles);
   localStorage.setItem("fleetData", JSON.stringify(vehicles));
 };
 
@@ -65,12 +75,66 @@ function calculateCharts() {
         },
       ],
     };
+
+  canvas.height=vehicles.length *100;
+
+   var options =  {
+    indexAxis: "y",
+    //barThickness:'flex' ,// 20,
+    //maxBarThickness:20,
+    //barPercentage:0.5,
+    //categoryercentage:1,
+    responsive: true,
+    maintainAspectRatio: false, // This will take css or js height, canvas element height
+  }
+
     new Chart(ctx, {
       type: "bar",
       data,
-      options: {
-        indexAxis: "y",
-      },
+      options
+    });
+
+
+    var currentEmissionIntensityData = vehicles.map((v) => {
+      // Check if the value is a number, if not, replace with 0
+      return typeof v.currentEmissionIntensity === 'number' && !isNaN(v.currentEmissionIntensity)
+          ? v.currentEmissionIntensity
+          : 0;
+    });
+  
+    var data = {
+      labels,
+      datasets: [
+        {
+          label: "Total Emission Intensity",
+          currentEmissionIntensityData,
+          fill: true,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 205, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(201, 203, 207, 0.2)",
+          ],
+          borderColor: [
+            "rgb(255, 99, 132)",
+            "rgb(255, 159, 64)",
+            "rgb(255, 205, 86)",
+            "rgb(75, 192, 192)",
+            "rgb(54, 162, 235)",
+            "rgb(153, 102, 255)",
+            "rgb(201, 203, 207)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    new Chart(ctx2, {
+      type: "bar",
+      data,
+      options
     });
   }
 }
