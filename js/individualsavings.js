19f78d0d52f80b29466a70a92e243148e9463d0c
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-03-14 17:51:27 -0400

    calculation for e85 ethanol usage and biofuel car and truck replacement

--- js/individualsavings.js
+++ js/individualsavings.js
@@ -1,14 +1,18 @@
-const individualSavingsBtn = document.querySelector('.action-savings a:nth-of-type(1)');
-const overallSavingsBtn = document.querySelector('.action-savings a:nth-of-type(2)');
+const individualSavingsBtn = document.querySelector(
+  ".action-savings a:nth-of-type(1)"
+);
+const overallSavingsBtn = document.querySelector(
+  ".action-savings a:nth-of-type(2)"
+);
 
-individualSavingsBtn.addEventListener('click', () => {
-    individualSavingsBtn.classList.add('active');
-    overallSavingsBtn.classList.remove('active');
+individualSavingsBtn.addEventListener("click", () => {
+  individualSavingsBtn.classList.add("active");
+  overallSavingsBtn.classList.remove("active");
 });
 
-overallSavingsBtn.addEventListener('click', () => {
-    overallSavingsBtn.classList.add('active');
-    individualSavingsBtn.classList.remove('active');
+overallSavingsBtn.addEventListener("click", () => {
+  overallSavingsBtn.classList.add("active");
+  individualSavingsBtn.classList.remove("active");
 });
 
 var fleetData = getFleetData();
@@ -17,44 +21,56 @@ var ev_data = [];
 
 window.onload = async () => {
   ev_data = await getEVData();
+  fleetData.forEach(async (item) => {
+    await evaluateSavings(item, ev_data);
+  })
   populateContainer();
-  localStorage.setItem('fleetData', JSON.stringify(fleetData));
+  console.log(fleetData);
+  localStorage.setItem("fleetData", JSON.stringify(fleetData));
 };
 
 function populateContainer() {
-    const container = document.querySelector(".vehicle-list");
-    fleetData.forEach(item => {
-        // Create divs for each item
-        const div = document.createElement("div");
-        div.classList.add("vehicle-item");
-        // Add 2 side by side divs of equal width for the description and options
-        let descDiv = createDescriptionDiv(item);
-        div.appendChild(descDiv);
-        container.appendChild(div);
-        // the options div should only be visible when either the row is hovered or clicked
-        div.addEventListener("click", async () => {
-          await calculateSavings(item);
-                const infoDiv = document.querySelector(".savings-info");
-                document.querySelectorAll('.vehicle-item').forEach(item => {
-                    item.classList.remove('selected');
-                });
-                div.classList.add('selected');
-                infoDiv.classList.remove('hidden');
-                document.querySelector('.percentage-value').innerHTML= item['percent_savings'];
-                document.querySelector('.savings-value').innerHTML= item['savings'];
-                console.log(item['selectedOption']);
-                document.querySelector('.option-selected').innerHTML = "Option Selected :" + item['selectedOption'];          
-        });
+  const container = document.querySelector(".vehicle-list");
+  fleetData.forEach((item) => {
+    // Create divs for each item
+    const div = document.createElement("div");
+    div.classList.add("vehicle-item");
+    // Add 2 side by side divs of equal width for the description and options
+    let descDiv = createDescriptionDiv(item);
+    div.appendChild(descDiv);
+    container.appendChild(div);
+    // the options div should only be visible when either the row is hovered or clicked
+    div.addEventListener("click", async () => {
+      const infoDiv = document.querySelector(".savings-info");
+      document.querySelectorAll(".vehicle-item").forEach((item) => {
+        item.classList.remove("selected");
+      });
+      div.classList.add("selected");
+      infoDiv.classList.remove("hidden");
+      document.querySelector(".percentage-value").innerHTML =
+        item["percent_savings"];
+      document.querySelector(".savings-value").innerHTML = item["savings"];
+      document.querySelector(".selected-option").innerHTML =item["selectedOption"];
     });
-    document.querySelectorAll('.vehicle-item')[0].click();
+  });
+  document.querySelectorAll(".vehicle-item")[0].click();
+  console.log(fleetData);
 }
 
 function createDescriptionDiv(item) {
-    const descDiv = document.createElement("div");
-    descDiv.classList.add("description");
-    descDiv.textContent = item['description'] + " - " + item['type'] + " - " +
-        item['year'] + " - " + item['make'] + " - " + item['model'];
-    return descDiv;
+  const descDiv = document.createElement("div");
+  descDiv.classList.add("description");
+  descDiv.textContent =
+    item["description"] +
+    " - " +
+    item["type"] +
+    " - " +
+    item["year"] +
+    " - " +
+    item["make"] +
+    " - " +
+    item["model"];
+  return descDiv;
 }
 
 function calculateEmissions(fleet) {
@@ -62,33 +78,33 @@ function calculateEmissions(fleet) {
   current_annual_emission = fleet["annualFuel"] * emmisionCoefficient;
   current_emission_intensity = current_annual_emission / fleet["annualVKT"];
   total_current_emissions = current_annual_emission * fleet["quantity"];
-  fleet['currentFuelEfficiency'] = current_fuel_efficiency;
-  fleet['currentEmissionIntensity'] = current_emission_intensity;
-  fleet['totalCurrentEmissions'] = total_current_emissions;
-  fleet['currentAnnualEmissions'] = (current_annual_emission/44.01);
+  fleet["currentFuelEfficiency"] = current_fuel_efficiency;
+  fleet["currentEmissionIntensity"] = current_emission_intensity;
+  fleet["totalCurrentEmissions"] = total_current_emissions;
+  fleet["currentAnnualEmissions"] = current_annual_emission / 44.01;
 }
 
-carsClassId = ["T", "I", "S","C","M","L","WS","WM"];
+carsClassId = ["T", "I", "S", "C", "M", "L", "WS", "WM"];
 
-async function calculateSavings(item) {  
-  let optionOpted = item['selectedOption'];
+async function calculateSavings(item) {
+  let optionOpted = item["selectedOption"];
   if (optionOpted == "Nothing") {
-    item['percent_savings'] = 0;
-    item['savings'] = 0;
+    item["percent_savings"] = 0;
+    item["savings"] = 0;
     return;
   } else {
-    item['savings'] = 12;
+    item["savings"] = 12;
     evaluateSavings(item, ev_data);
   }
 }
 
 
 function goToResultsPage() {
-    localStorage.setItem('fleetData', JSON.stringify(fleetData)); // Update the local storage data
-    window.location.href = ".html";
+  localStorage.setItem("fleetData", JSON.stringify(fleetData)); // Update the local storage data
+  window.location.href = ".html";
 }
 
 function goToGreenOptions() {
-    localStorage.setItem('fleetData', JSON.stringify(fleetData)); // Update the local storage data
-    window.location.href = "/green-options.html";
+  localStorage.setItem("fleetData", JSON.stringify(fleetData)); // Update the local storage data
+  window.location.href = "/green-options.html";
 }
--- js/savings.js
+++ js/savings.js
@@ -3,8 +3,7 @@ var truckTypes = ['PS','PL','US','UL','V','VC','VP','SP']
 var fuelTypes = {
     'Gasoline':'X',
     "E10 Gasoline":'Z',
-    'Diesel':'D',
-    'Electric':'B',
+    'Diesel':'D'
 }
 var emmisionCoefficient = localStorage.getItem("emissionCoefficient") || "30";
 
@@ -40,6 +39,70 @@ function evaluateSavings(item, ev_data) {
         
         item['savings'] = (selectedEV['total_emissions_savings'] / 1000000).toFixed(2);
  
+        return item;
+    }
+    if(optionOpted == 'E85 Ethanol Usage') {
+        item['percent_savings'] = 79;
+        item['savings'] =(( 0.8 * item['currentAnnualEmissions'])/1000000).toFixed(2);
+        return item;
+    }
+
+    if (optionOpted == 'Replace with Biofuel car')  {
+        ev_data = ev_data.filter(x => x['VehicleTypeId'] == 'ICE');
+        ev_data = ev_data.filter(x => carTypes.includes(x['ClassId']));
+        ev_data = ev_data.filter(x => x['FuelTypeId'] == 'E');
+        ev_data = ev_data.sort((x,y) => x['RankingAll'] - y['RankingAll']);
+        ev_data = ev_data.slice(0,10);
+
+        ev_data.map(element => {
+            element.electical_efficiency = (element['CombGasConsumption']  ) / 100;
+            element.ev_emissions_intensity = element.electical_efficiency * emmisionCoefficient;
+            element.savings = ((item['currentEmissionIntensity'] - element.ev_emissions_intensity)/item['currentEmissionIntensity'] );
+            element.percent_savings = (element.savings * 100);
+            element.total_emissions_savings = element.savings * item['currentAnnualEmissions']
+            element.new_annual_emissions = item['currentAnnualEmissions'] - element.total_emissions_savings
+        });
+        
+        ev_data = ev_data.sort((x,y) => x['total_emissions_savings'] - y['total_emissions_savings']);
+        let selectedEV = ev_data[0];
+        console.log(selectedEV);
+        if(selectedEV) {
+            item['percent_savings'] = Math.trunc(selectedEV['percent_savings']);
+            item['savings'] = (selectedEV['total_emissions_savings'] / 1000000).toFixed(2);    
+        }
+        else {
+            item['percent_savings'] = 0;
+            item['savings'] = 0;
+        }
+        return item;
+    }
+
+    if(optionOpted == 'Replace with Biofuel Truck')  {
+        ev_data = ev_data.filter(x => x['VehicleTypeId'] == 'ICE');
+        ev_data = ev_data.filter(x => truckTypes.includes(x['ClassId']));
+        ev_data = ev_data.filter(x => x['FuelTypeId'] == 'E');
+        ev_data = ev_data.sort((x,y) => x['RankingAll'] - y['RankingAll']);
+        ev_data = ev_data.slice(0,10);
+
+        ev_data.map(element => {
+            element.electical_efficiency = (element['CombGasConsumption']  ) / 100;
+            element.ev_emissions_intensity = element.electical_efficiency * emmisionCoefficient;
+            element.savings = ((item['currentEmissionIntensity'] - element.ev_emissions_intensity)/item['currentEmissionIntensity'] );
+            element.percent_savings = (element.savings * 100);
+            element.total_emissions_savings = element.savings * item['currentAnnualEmissions']
+            element.new_annual_emissions = item['currentAnnualEmissions'] - element.total_emissions_savings
+        });
+        
+        ev_data = ev_data.sort((x,y) => x['total_emissions_savings'] - y['total_emissions_savings']);
+        let selectedEV = ev_data[0];
+        if(selectedEV) {
+            item['percent_savings'] = Math.trunc(selectedEV['percent_savings']);
+            item['savings'] = (selectedEV['total_emissions_savings'] / 1000000).toFixed(2);    
+        }
+        else {
+            item['percent_savings'] = 0;
+            item['savings'] = 0;
+        }
         return item;
     }
 }
